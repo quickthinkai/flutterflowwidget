57583ce84171cc612512f876aaf20738bbef38cd
Ahmad Raza Ahmad <80081771+quickthinkai@users.noreply.github.com>
2024-07-14 15:12:12 +0500

    Create NewCustomWidget.dart

---
+++ NewCustomWidget.dart
@@ -0,0 +1,52 @@
+// Automatic FlutterFlow imports
+import '/backend/schema/structs/index.dart';
+import '/backend/schema/enums/enums.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/widgets/index.dart'; // Imports other custom widgets
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+//import '/backend/schema/structs/index.dart';
+
+//import '/backend/schema/structs/index.dart';
+
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class NewCustomWidget extends StatefulWidget {
+  const NewCustomWidget({
+    super.key,
+    this.width,
+    this.height,
+    this.text1,
+  });
+
+  final double? width;
+  final double? height;
+  final String? text1;
+
+  @override
+  State<NewCustomWidget> createState() => _NewCustomWidgetState();
+}
+
+class _NewCustomWidgetState extends State<NewCustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: widget.width,
+        height: widget.height,
+        child: GradientText(
+          widget.text1!,
+          style: TextStyle(
+            fontSize: 40.0,
+          ),
+          colors: [
+            Color.fromARGB(255, 7, 120, 213),
+            const Color.fromARGB(255, 138, 10, 1),
+            Color.fromARGB(255, 215, 1, 204),
+          ],
+        ));
+  }
+}
